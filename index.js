a688acaf2fdb752ce5c041a950913c16ff1aee45
eiji0111 <ee111fender@gmail.com>
2021-07-24 10:03:09 +0900

    [Update]dev

---
+++ index.js
@@ -0,0 +1,16 @@
+const hello = "Hello world!";
+console.log(hello);
+
+const walk = {
+  action: "歩く"
+}
+console.log(walk.action);
+
+walk.action = "あるく";
+console.log(walk.action);
+
+// const walk = { run = "走る" } はできない
+
+array = [1,2,3,4,5];
+
+array.forEach(number => number);
